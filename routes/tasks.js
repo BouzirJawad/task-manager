8f4be8f6f4e17ce30f84822b72ea558d76f1756b
BouzirJawad <jawadbouzir01@gmail.com>
2024-12-27 11:21:56 +0100

    added 2 routers (post router, update router)

--- routes/tasks.js
+++ routes/tasks.js
@@ -16,4 +16,40 @@ router.post('/', async (req, res)=>{
     }
 });
 
+//display all tasks
+router.get('/', async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.json(tasks);
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+});
+
+//find task by id
+router.get('/:id', async (req, res)=>{
+    try {
+        const taskByID = await Task.findById(req.params.id)
+        if (!taskByID) {
+            return res.status(404).json({error: 'Task not found'})
+        }
+        res.json(taskByID);
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+});
+
+//update task by id
+router.patch('/:d', async (req, res)=>{
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if (!task) {
+            return res.status(404).json({error: 'Task not found'});
+        }
+        res.json(task)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}),
+
 module.exports = router;
